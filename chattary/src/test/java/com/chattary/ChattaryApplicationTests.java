0a0065fd4b1c3adac5036da61fc370b2dead7123
Kimchanseul <kimseongeun526@google.com>
2025-12-15 18:12:46 +0900

    project create

---
+++ chattary/src/test/java/com/chattary/ChattaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chattary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
