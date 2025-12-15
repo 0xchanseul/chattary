19222558e340483058fd4ffe76082d58d82e5032
Kimchanseul <kimseongeun526@google.com>
2025-12-15 18:12:46 +0900

    project create

---
+++ chattary/src/main/java/com/chattary/ChattaryApplication.java
@@ -0,0 +1,13 @@
+package com.chattary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattaryApplication.class, args);
+	}
+
+}
